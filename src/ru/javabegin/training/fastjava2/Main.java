c11d4f1fddc2669059015432baf98a9187458da1
Val <vr_89@mail.ru>
2016-08-23 20:20:34 +0400

    Commit

--- src/ru/javabegin/training/fastjava2/Main.java
+++ src/ru/javabegin/training/fastjava2/Main.java
@@ -4,11 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(faktorial(5));
+        System.out.println(faktorial(0));
+        System.out.println(faktorial(1));
+        System.out.println(faktorial(2));
+        System.out.println(faktorial(10));
     }
     public static int faktorial(int n) {
         int i = 2;
-        if (!(n == 2)) i = n * faktorial(n-1);
+        if(n == 0) return 0;
+        else if (n == 1) return 1;
+        else if (!(n == 2)) i = n * faktorial(n-1);
         return i;
     }
 }
